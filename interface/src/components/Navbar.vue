bacbaba6e69cf8f861ec6222602a45b591f03393
Diego Porto <dportosilva@gmail.com>
2020-06-15 03:54:55 +0100

    Update

---
+++ interface/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-app-bar flat color="black" >
+      <v-app-bar-title
+        style="cursor: pointer"
+        @click="$router.push('/')"
+        class="red--text"
+        router to="/"
+
+      >
+        <v-icon left color="red">mdi-music</v-icon>
+        <span class="title font-black" color="white">UMinhoMusic</span>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn 
+        text 
+        color="white"
+        @click="$router.push('/about')" 
+        class="font-weight-light"
+      >
+        <span>About</span>
+        <v-icon right color="white">mdi-information-outline</v-icon>
+      </v-btn>
+
+      <v-btn 
+        text 
+        color="white"
+        @click="$router.push('/team')"  
+        class="font-weight-light"
+      >
+        <span>Team</span>
+        <v-icon right color="white">mdi-account-group</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
---
+++ interface/src/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app id="home">
+        <Navbar />
+        <v-content>
+            <v-container fluid style="padding: 0%">
+                <v-img src="../assets/topo.png" height="210px">
+                    <div>
+                        <p style="color:white">Teste</p>
+                    </div>
+                </v-img>    
+            </v-container>
+        </v-content>
+    </v-app>    
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+
+export default {
+    name: 'home',
+    components: {
+        Navbar
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
